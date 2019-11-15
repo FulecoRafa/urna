54d7bae7d6d4f7d38d45fee6ee5c919541c5c0e5
Rafael Oliveira <ra.pha@live.com>
2019-11-14 21:05:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+
+let electionVector = [
+    "Eleição 1",
+    "Eleição 2",
+    "Eleição 3"
+]
+
+function renderElections(){
+    const elections = document.querySelector(".elections");
+    elections.innerHTML = "";
+    for(el of electionVector){
+        let listElement = document.createElement('li');
+        let nameButton = document.createElement('button');
+        let excludeButton = document.createElement('button');
+        const x = document.createTextNode("X");
+        let name = document.createTextNode(el);
+        let empty = document.createTextNode(" ");
+
+        listElement.classList = "election";
+        excludeButton.classList = "exclude";
+        nameButton.classList = "name";
+
+        let index = electionVector.indexOf(el);
+        excludeButton.setAttribute('onclick' , 'excludeElection('+index+')');
+
+        excludeButton.appendChild(x);
+        nameButton.appendChild(name);
+        listElement.appendChild(nameButton);
+        listElement.appendChild(empty);
+        listElement.appendChild(excludeButton);
+        
+        elections.appendChild(listElement);
+    }
+}
+
+renderElections();
+
+function excludeElection(index){
+    electionVector.splice(index,1);
+    renderElections();
+}
+
+function electionAdd(){
+    const electionAddForm = document.querySelector(".electionAdder");
+    let name = document.querySelector(".electionAdder input[type = text]");
+    electionVector.push(name.value);
+    name.value = "";
+    renderElections();
+}
