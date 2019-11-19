67a031560826c634cd0f1a85b9a9a9edf364655b
Rafael Oliveira <ra.pha@live.com>
2019-11-19 09:01:33 -0300

    Finish (no photo/no JSON)

--- election.js
+++ election.js
@@ -146,3 +146,126 @@ function candidateFormAppear(){
     let form = document.querySelector(".candidateAdder");
     form.style.visibility = "visible";
 }
+
+//Start Election
+//---------------------------------
+function startElection(){
+    for(el of electionVector[selectedIndex].candidates){
+        el.votes = 0;
+    }
+    let votingMachine = document.querySelector("#votingMachine");
+    votingMachine.style.visibility = "visible"
+    renderOptions();
+}
+
+function renderOptions(){
+    let defaultOption = document.createElement('option');
+    let defaultText = document.createTextNode("Candidato");
+    let selector = document.querySelector("#candidateVote");
+    defaultOption.appendChild(defaultText);
+    selector.appendChild(defaultOption);
+    for(el of electionVector[selectedIndex].candidates){
+        let option = document.createElement('option');
+        option.setAttribute('value' , el.num);
+        let text = document.createTextNode(el.num);
+        option.appendChild(text);
+
+        selector.appendChild(option);
+    }
+}
+
+function addVote(){
+    let option = document.querySelector("#candidateVote");
+    for(el of electionVector[selectedIndex].candidates){
+        if(el.num == option.value){
+            el.votes++;
+            break;
+        }
+    }
+    option.value="Candidato";
+}
+
+function countVotes(){
+    let votingArray = [];
+    for(el of electionVector[selectedIndex].candidates){
+        votingArray.push(el);
+    }
+    let ordered = false;
+    while(!ordered){
+        ordered = true;
+        for(let i = 0 ; i < votingArray.length-1 ; i++){
+            if(votingArray[i].votes < votingArray[i+1].votes){
+                let aux = votingArray[i];
+                votingArray[i] = votingArray[i+1];
+                votingArray[i+1] = aux;
+                ordered = false;
+            }
+        }
+    }
+    document.querySelector("#endElection").style.visibility = "visible";
+    console.log(votingArray);
+    showResults(votingArray);
+    document.querySelector("#votingMachine").style.visibility="hidden";
+}
+
+function showResults(votingArray){
+    for(let i = 0 ; i < votingArray.length ; i++){
+        let row = document.createElement('tr');
+        let name = document.createElement('td');
+        let num = document.createElement('td');
+        let votes = document.createElement('td');
+        let position = document.createElement('td');
+
+        let nameText = document.createTextNode(votingArray[i].name);
+        let numText = document.createTextNode(votingArray[i].num);
+        let votesText = document.createTextNode(votingArray[i].votes);
+        let positionText = document.createTextNode(i+1+'º');
+
+        name.appendChild(nameText);
+        num.appendChild(numText);
+        votes.appendChild(votesText);
+        position.appendChild(positionText);
+
+        row.appendChild(name);
+        row.appendChild(num);
+        row.appendChild(votes);
+        row.appendChild(position);
+
+        document.querySelector("#endElection table").appendChild(row);
+    }
+}
+
+function endOfAll(){
+    document.querySelector("#endElection").style.visibility = "hidden";
+    let table = document.querySelector("#endElection table");
+    table.innerHTML = "";
+    
+    let caption = document.createElement('caption');
+    let captionText = document.createTextNode("Resultados da eleição:");
+    caption.appendChild(captionText);
+    table.appendChild(caption);
+
+    let row = document.createElement('tr');
+
+    let name = document.createElement('th');
+    let num = document.createElement('th');
+    let votes = document.createElement('th');
+    let position = document.createElement('th');
+
+    let nameText = document.createTextNode("Nome:");
+    let numText = document.createTextNode("Número:");
+    let votesText = document.createTextNode("Quantidade de votos:");
+    let positionText = document.createTextNode("Posição:");
+
+    name.appendChild(nameText);
+    num.appendChild(numText);
+    votes.appendChild(votesText);
+    position.appendChild(positionText);
+
+    row.appendChild(name);
+    row.appendChild(num);
+    row.appendChild(votes);
+    row.appendChild(position);
+
+    table.appendChild(row);
+}
